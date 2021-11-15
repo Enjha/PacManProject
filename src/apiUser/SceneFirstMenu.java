921bcb5be34bc7b77f3eef40247e8a8727dfe9d2
loic_fery <loic-fery@orange.fr>
2021-11-15 17:41:34 +0100

    debut premier menu

--- src/apiUser/SceneFirstMenu.java
+++ src/apiUser/SceneFirstMenu.java
@@ -1,4 +1,20 @@
 package apiUser;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
 public class SceneFirstMenu implements ScenePacMan{
+
+    private BorderPane root = new BorderPane();
+    private final int widht = 400;
+    private final int height = 300;
+    private Scene scene = new Scene(root, widht, height);
+
+    private void init(){
+
+    }
+
+    public Scene getScene(){
+        return scene;
+    }
 }
