63d0537969eb00746b4d08a534ad19215bca2864
n17019061 <thomas.ninci@etu.univ-amu.fr>
2021-11-22 16:18:26 +0100

    EventListener < FxListener

--- src/engines/graphic/EventListener.java
+++
@@ -1,9 +0,0 @@
-package engines.graphic;
-
-import gameplay.Entity;
-
-public interface EventListener {
-
-    void onEvent(String event);
-    void onEntityEvent(Entity entity, String eventName);
-}
