cae2bf5977760b3892763874398bb5b4836d7b2e
Enjha <proxzy-yann@hotmail.fr>
2021-11-26 19:58:04 +0100

    Début de la classe du moteur graphique

--- src/engines/graphic/ClassicConvertSceneToGraphic.java
+++ src/engines/graphic/ClassicConvertSceneToGraphic.java
@@ -1,8 +1,31 @@
 package engines.graphic;
 
+import apiUser.SetupScene;
+import engines.graphic.view.WallView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import scene.Scene;
+import scene.SceneCase;
 
-public class ClassicConvertSceneToGraphic implements ConvertSceneToGraphic{
+import java.io.File;
 
-    public void test(Scene scene){}
+public class ClassicConvertSceneToGraphic implements ConvertSceneToGraphic {
+
+    public void test(Scene scene) {
+        double TEXTURE_SIZE = 32.0;
+        double CASE_SIZE = 10.0;
+        SceneCase[][] sceneCases = scene.getCases();
+        SetupScene setUp = new SetupScene();
+        int x = 0;
+        int y = 0;
+        for (SceneCase[] sceneCases1 : sceneCases) {
+            for (SceneCase sceneCase : sceneCases1) {
+                //On parcours les cases en récuperant pour chaque case ses composants et les afficher
+                Image texture_wall = new WallView().getWallView(sceneCase);
+                setUp.setImageView(new ImageView(), x * CASE_SIZE, y * CASE_SIZE, TEXTURE_SIZE, TEXTURE_SIZE, texture_wall, true);
+                y++;
+            }
+            x++;
+        }
+    }
 }
