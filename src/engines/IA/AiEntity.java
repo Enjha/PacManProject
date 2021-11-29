0cb8acd8b1887aeb5f59fcc24ba6cd44b3c91315
n17019061 <thomas.ninci@etu.univ-amu.fr>
2021-11-29 15:15:27 +0100

    Plan de l'IA

--- src/engines/IA/AiEntity.java
+++ src/engines/IA/AiEntity.java
@@ -1,48 +1,80 @@
 package engines.IA;
 
+import gameplay.Direction;
 import gameplay.Entity;
 import pacman.Ghost;
 import pacman.Pacman;
 
-import javax.swing.text.Position;
+public class AiEntity implements Runnable{
 
-public class AiEntity {
-
-    private int choice;
     private Entity entity;
+    private Ghost ghost;
+    private Pacman pacman;
+    private boolean isAbleToFollow;
 
     public AiEntity(Entity entity, Ghost ghost, Pacman pacman){
         this.entity = entity;
-        choice = calculateDijkstra(ghost,pacman);
-        pickDirection();
+        this.ghost = ghost;
+        this.pacman = pacman;
+        isAbleToFollow = true;
+        moove();
     }
 
-    private int calculateDijkstra(Ghost ghost, Pacman pacman){
+    private void moove(){
+        ghost.setDirection(Direction.North);
+        while(ghost.isAlive()){
+            if(algoDijkstra() <= 7 && isAbleToFollow){ //ghost va passer dans l'état de déplacement "poursuite"
+                followPM();
+                isAbleToFollow = false;
+                Thread thread = new Thread();
+                thread.start();
+            }
+            else{ //ghost va rester dans un état de déplacement "classique"
+
+            }
+        }
+    }
+
+    private int algoDijkstra(){
+        int distance = 0;
 
 
-        return 1;
+        return distance;
+    }
+
+    private void followPM(){
+
     }
 
     private void pickDirection(){
+        int choice = 1 + (int)(Math.random() * 5);
         switch(choice){
-            case 0 :
-
-                break;
             case 1:
-
+                ghost.setDirection(Direction.North);
                 break;
             case 2:
-
+                ghost.setDirection(Direction.East);
                 break;
             case 3:
-
+                ghost.setDirection(Direction.South);
+                break;
+            case 4:
+                ghost.setDirection(Direction.West);
                 break;
             default :
-
+                System.err.println("Error with direction in AI motor");
                 break;
         }
     }
 
 
-
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(5000);
+            isAbleToFollow = true;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
