e1b3cb0c504186fe2355c3ceb62001e771962832
Enjha <proxzy-yann@hotmail.fr>
2021-12-01 16:55:45 +0100

    avancé apparence

--- src/pacman/scene/SceneLabyrinthMenu.java
+++ src/pacman/scene/SceneLabyrinthMenu.java
@@ -2,11 +2,18 @@ package pacman.scene;
 
 import apiUser.SetupScene;
 import engines.graphic.GraphicEngine;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 
 public class SceneLabyrinthMenu implements ScenePacMan {
@@ -20,13 +27,29 @@ public class SceneLabyrinthMenu implements ScenePacMan {
     private Pane panel;
     private GraphicEngine graphicEngine;
 
-    public SceneLabyrinthMenu(Stage stage, GraphicEngine graphicEngine){
+    public SceneLabyrinthMenu(Stage stage, GraphicEngine graphicEngine) {
         this.stage = stage;
         this.graphicEngine = graphicEngine;
     }
 
-    public Scene getScene(){
+    public Scene getScene() {
         panel = new AnchorPane();
+        root.setId("background");
+        File f = new File("ressources/styles/menu_style.css");
+        scene.getStylesheets().clear();
+        scene.getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\", "/"));
+
+        Button buttonSetting = new Button();
+        buttonSetting.setId("buttonSetting");
+        Image image = new Image(new File("ressources/textures/setting.png").toURI().toString());
+        ImageView icon = new ImageView(image);
+        icon.setFitWidth(50);
+        icon.setFitHeight(50);
+        buttonSetting.setGraphic(icon);
+        setupScene.setButton(buttonSetting, "", Pos.CENTER, 20, 20, 80, 80, new Font(30), true);
+        buttonSetting.setOnMouseClicked((event) -> setSceneOption());
+
+        panel.getChildren().addAll(buttonSetting);
         root.getChildren().add(panel);
         root.setStyle("-fx-background-color: black;");
         graphicEngine.setControlEngineState(true);
@@ -34,11 +57,16 @@ public class SceneLabyrinthMenu implements ScenePacMan {
         return scene;
     }
 
-    public Pane getPanel(){
+    public Pane getPanel() {
         return panel;
     }
 
-    public boolean isSceneGame(){
+    public boolean isSceneGame() {
         return true;
     }
+
+    private void setSceneOption() {
+        graphicEngine.setCurrentScene(new SceneOptionMenu(stage, graphicEngine));
+    }
+
 }
